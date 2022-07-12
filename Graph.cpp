a4917234aa0dc22fa2fae0d136990c04676e49d0
KGYeow <kokguan0819@gmail.com>
2022-07-12 20:34:03 +0800

    Modify arrangement, add weight
    Made some small modification on the structure arrangement and add the weight distance.

--- Graph.cpp
+++ Graph.cpp
@@ -1,79 +1,118 @@
 // Adjacency Matrix representation in C++
 
 #include <iostream>
+#include <limits>
 using namespace std;
 
-class Graph {
-   private:
-  bool** adjMatrix;
-  int numVertices;
-  int Distance[5][5] = {{0,1,2,3,4}, {4,5,6,7,8}, {8,9,10,11,12},{11,12,13,14,15},{14,15,16,17,18}};
+class Graph
+{
+	private:
+  		bool** adjMatrix;    // Indicates if there is an edge exist between two cities.
+ 		int numVertices,     // Stores the number of cities.
+		 	distance[5][5];  // Stores the distance from a city to the other city.
 
-   public:
-  // Initialize the matrix 
-  Graph(int numVertices) {
-    this->numVertices = numVertices;
-    adjMatrix = new bool*[numVertices];
-    for (int i = 0; i < numVertices; i++) {
-      adjMatrix[i] = new bool[numVertices];
-    }
-    resetGraph();
-  }
+   	public:
+   		
+  	// Initialize the adjacency matrix
+	Graph(int numVertices)
+	{
+  		this->numVertices = numVertices;
+    	adjMatrix = new bool*[numVertices];
+    	
+    	for (int i = 0; i < numVertices; i++)
+      		adjMatrix[i] = new bool[numVertices];
+      		
+    	resetGraph();
+  	}
 
-  // Add edges
-  void addEdge(int i, int j) {
-    adjMatrix[i][j] = true;
-  }
+	// Add edges
+  	void addEdge(int i, int j, int weight)
+	{
+    	adjMatrix[i][j] = true;
+    	distance[i][j] = weight;
+  	}
 
-  // Remove edges
-  void removeEdge(int i, int j) {
-    adjMatrix[i][j] = false;
-  }
+  	// Remove edges
+  	void removeEdge(int i, int j)
+	{
+   		adjMatrix[i][j] = false;
+  	}
   
-  // Get adjMatrix[i][j]
-  bool areAdjacent(int i, int j){
-  	return adjMatrix[i][j];
-  }
+  	// Get adjMatrix[i][j] or check if city j is adjacent to city i.
+  	bool areAdjacent(int i, int j)
+  	{
+  		return adjMatrix[i][j];
+  	}
   
-  // Get Distance[i][j]
-  int getDistance(int i, int j){
-  	return Distance[i][j];
-  }
+  	// Get Distance[i][j]
+	int getDistance(int i, int j)
+	{
+	  	return distance[i][j];
+	}
 
-  // Print the martix
-  void toString() {
-    for (int i = 0; i < numVertices; i++) {
-      cout << i << " : ";
-      for (int j = 0; j < numVertices; j++)
-        cout << adjMatrix[i][j] << " ";
-      cout << "\n";
-    }
-  }
-  
-  //Initialise and reset the graph
-  void resetGraph() {
-  	for (int i = 0; i < numVertices; i++){
-  		for (int j = 0; j < numVertices; j++){
-  			adjMatrix[i][j] = false;
-		  }
-	  }
-	addEdge(0, 1);
-	addEdge(0, 2);
-	addEdge(1, 2);
-	addEdge(2, 0);
-	addEdge(2, 3);
-  }
+	// Print the martix
+	void toString() {
+		for (int i = 0; i < numVertices; i++)
+		{
+			cout << i << " : ";
+		  	for (int j = 0; j < numVertices; j++)
+		    	cout << adjMatrix[i][j] << " ";
+		  	cout << endl;
+		}
+	}
 
-  ~Graph() {
-    for (int i = 0; i < numVertices; i++)
-      delete[] adjMatrix[i];
-    delete[] adjMatrix;
-  }
+	//Initialise and reset the graph
+	void resetGraph()
+	{
+		for (int i = 0; i < numVertices; i++)
+		{
+			for (int j = 0; j < numVertices; j++)
+				adjMatrix[i][j] = false;
+	  	}
+	  	
+	  	// Add the edges and their corresponding distance/weight to the adjacency matrix.
+	  	int PA=0, LO=1, LV=2, SY=3, TO=4;		
+		addEdge(PA, TO, 9715);
+		addEdge(PA, LO, 343);
+		addEdge(TO, SY, 7826);
+		addEdge(LV, SY, 12434);
+		addEdge(LV, LO, 8402);
+		
+		// In the adjacency matrix, if the edge is not present, then it will be infinity. For same node, it will be 0.
+		for (int i = 0; i < numVertices; i++)
+		{
+			for (int j = 0; j < numVertices; j++)
+			{
+				if (adjMatrix[i][j] == false)
+					adjMatrix[i][j] = numeric_limits<int>::infinity();
+			}
+	  	}
+	}
+
+  	~Graph()
+	{
+    	for (int i = 0; i < numVertices; i++)
+      		delete[] adjMatrix[i];
+    	delete[] adjMatrix;
+  	}
 };
 
-int main() {
-  Graph g(5);
+int main()
+{
+	// Initialize a directed and weighted graph when this program starts up.
+	Graph g(5);
 
-  g.toString();
+	g.toString();
+	
+	// Display the distance
+	cout << endl;
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+			cout << g.getDistance(i, j) << "\t";
+		cout << endl;	
+	}
+	
+	return 0;
 }
 
